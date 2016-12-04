26971915b7392813460c0d7921082007c7e4a066
daniel.cantor@gmail.com <friday11>
2016-12-03 22:01:35 -0500

    test

--- lists/list-order1.py
+++ lists/list-order1.py
@@ -19,8 +19,26 @@ print animals
 print animals.count("badger")
 
 # Remove objects from the list
-
 animals.remove("emu")
-
 print animals
+
+# Reverse the list
+animals.reverse()
+print animals
+
+
+list1=["dog", "cat", "mouse"]
+list2=["doge", "cat", "mouse"]
+print list1
+print list2
+
+print cmp(list1, list2)
+
+# list operations
+newlist1 = list1 * 4
+print newlist1
+
+# for fun
+for x in newlist1: print x
+
 
