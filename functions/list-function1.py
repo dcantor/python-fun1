fbe36bcb0fb389c0023488d35c696b053f2bd4e6
daniel.cantor@gmail.com <friday11>
2016-12-03 23:05:22 -0500

    test

--- functions/list-function1.py
+++ functions/list-function1.py
@@ -77,3 +77,16 @@ sum = lambda arg1, arg2: arg1 + arg2;
 # Now you can call sum as a function
 print "Value of total : ", sum(10, 20)
 print "Value of total : ", sum(20, 20)
+
+
+
+def sum( arg1, arg2 ):
+   # Add both the parameters and return them."
+   total = arg1 + arg2
+   print "Inside the function : ", total
+   return total;
+
+# Now you can call sum function
+total = sum( 10, 20 );
+print "Outside the function : ", total
+
