eee08c18a6a0f0bc925fc5f461c5af31c2409688
daniel.cantor@gmail.com <friday11>
2016-12-03 22:25:38 -0500

    test

---
+++ dictionaries/dictionary2.py
@@ -0,0 +1,32 @@
+my_dict = {
+    "food" : ["chocolate", "soup", "salad", "cheese"],
+    "cash" : 4056,
+    "day" : "saturday"
+}
+
+print my_dict
+print
+
+print my_dict["food"][0]
+print my_dict["food"][1]
+
+
+print my_dict["day"]
+
+my_dict.clear()
+print my_dict
+
+my_dict = {
+    "food" : ["chocolate", "soup", "salad", "cheese"],
+    "cash" : 99999,
+    "day" : "saturday"
+}
+
+print my_dict
+print len(my_dict)
+print str(my_dict)
+print type(my_dict)
+
+zoo_animals = ["pangolin", "cassowary", "sloth", "cat" ];
+print type(zoo_animals)
+
